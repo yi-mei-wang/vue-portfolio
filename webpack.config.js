c4f0affb9a65594a20a47370bc04fcf8816b1d36
mei <yimei.wym@gmail.com>
2019-06-12 23:37:02 +0800

    remove link from config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,6 @@ module.exports = {
     publicPath: "/dist/",
     filename: "build.js"
   },
-  link: [
-    {
-      rel: "icon",
-      type: "image/x-icon",
-      href: "static/favicon-16x16.png"
-    }
-  ],
   module: {
     rules: [
       {
